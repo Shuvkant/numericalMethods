97a89d983a0d94f6fcb75ddd0d741f4714b2c2d6
Shuvkant <weird426344@gmail.com>
2024-01-04 23:47:44 +0545

    adding gaussElimination

--- gaussElimination.cpp
+++ gaussElimination.cpp
@@ -6,34 +6,45 @@
 #define width 5
 #define max 100
 void Display(double matrix[max][max], int number_of_equations);
-void Result(double matrix[max][max], int number_of_equations) {
+
+// solution finding
+void Result(double matrix[max][max], int number_of_equations)
+{
   int n = number_of_equations;
   double y[max];
   y[n - 1] = matrix[n][n + 1] / matrix[n][n];
-  for (int i = n - 1; i >= 1; i--) {
+  for (int i = n - 1; i >= 1; i--)
+  {
     y[i - 1] = matrix[i][n + 1];
-    for (int j = i + 1; j <= n; j++) {
+    for (int j = i + 1; j <= n; j++)
+    {
       y[i - 1] = y[i - 1] - matrix[i][j] * y[j - 1];
     }
     y[i - 1] = y[i - 1] / matrix[i][i];
   }
-  for (int i = 1; i <= n; i++) {
+  for (int i = 1; i <= n; i++)
+  {
     std::cout << "x" << i << "=" << y[i - 1] << "\n";
   }
 }
-void Calculate(double matrix[max][max], int number_of_equations) {
+void Calculate(double matrix[max][max], int number_of_equations)
+{
   double ratio;
-  for (int i = 1; i <= number_of_equations; i++) {
-    for (int j = i + 1; j <= number_of_equations; j++) {
+  for (int i = 1; i <= number_of_equations; i++)
+  {
+    for (int j = i + 1; j <= number_of_equations; j++)
+    {
       ratio = matrix[j][i] / matrix[i][i];
-      for (int k = 1; k <= number_of_equations + 1; k++) {
+      for (int k = 1; k <= number_of_equations + 1; k++)
+      {
         matrix[j][k] = matrix[j][k] - ratio * (matrix[i][k]);
       }
     }
   }
 }
 
-int main() {
+int main()
+{
   int number_of_equations;
   int n;
   int i, j, k;
@@ -43,8 +54,10 @@ int main() {
   n = number_of_equations;
   double matrix[max][max];
   std::cout << "Enter the augmented matrix: " << std::endl;
-  for (i = 1; i <= number_of_equations; i++) {
-    for (j = 1; j <= number_of_equations + 1; j++) {
+  for (i = 1; i <= number_of_equations; i++)
+  {
+    for (j = 1; j <= number_of_equations + 1; j++)
+    {
       // std::cout << "A[" << i << "][" << j << "]=";
       std::cin >> matrix[i][j];
     }
@@ -53,20 +66,18 @@ int main() {
   Calculate(matrix, number_of_equations);
   Display(matrix, number_of_equations);
   Result(matrix, n);
-  // double x3 = matrix[3][4] / matrix[3][3];
-  // std::cout << "x3=" << x3;
-  // solution finding
 }
-void Display(double matrix[max][max], int number_of_equations) {
+void Display(double matrix[max][max], int number_of_equations)
+{
   // Displaying the Entered matrix;
   std::cout << "The matrix is:\n";
-  for (int i = 1; i <= number_of_equations; i++) {
-    for (int j = 1; j <= number_of_equations + 1; j++) {
+  for (int i = 1; i <= number_of_equations; i++)
+  {
+    for (int j = 1; j <= number_of_equations + 1; j++)
+    {
       std::cout << matrix[i][j] << "\t\t";
       // std::cin >> a[i][j];
     }
     std::cout << "\n";
   }
 }
-
-// Solution finding
